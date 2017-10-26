112eea1e794170bc53be02c9e873c091d741e01c
Piotrek <@>
2017-10-26 15:48:37 +0200

    przesłanie plików

---
+++ A_18/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] wektor1 = { 5, 6, -1 };
+            double[] wektor2 = { 0, 2, 3 };
+            double wynik = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                wynik += wektor1[i] * wektor2[i];
+            }
+            Console.WriteLine("Iloczyn skalarny: {0}", wynik);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+Napisz program obliczający iloczyn skalarny na podstawie współrzędnych dwóch wektorów w trójwymiarowej przestrzeni. Wynik wyświetl na konsoli.
+
+Przykładowe wejście:
+
+Wektor1: [5,6,-1]
+
+Wektor2: [0,2,3]
+
+Przykładowe wyjście:
+
+Iloczyn skalarny: 9
